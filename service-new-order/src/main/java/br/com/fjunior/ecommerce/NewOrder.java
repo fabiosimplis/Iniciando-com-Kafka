664b4018c636ab77628e10193af0e9c63b9f8440
fabiosimplis <fabio.simplis@gmail.com>
2024-07-14 18:10:38 -0300

    ADD Micro serviços de email e fast delegate real

--- service-new-order/src/main/java/br/com/fjunior/ecommerce/NewOrder.java
+++ service-new-order/src/main/java/br/com/fjunior/ecommerce/NewOrder.java
@@ -11,21 +11,14 @@ public class NewOrder {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         try(var orderDispacher = new KafkaDispatcher<Order>()) {
-            try (var emailDispacher = new KafkaDispatcher<Email>()) {
-
                 var email = Math.random() + "@email.com";
                 for (var i = 0; i < 10; i++) {
-
                     var orderId = UUID.randomUUID().toString();
                     var amount = new BigDecimal(Math.random() * 5000 + 1);
                     var order = new Order(orderId, amount, email);
                     var id = new CorrelationId(NewOrder.class.getSimpleName());
                     orderDispacher.send("ECOMMERCE_NEW_ORDER", email, id, order);
-
-                    Email emailMessage = new Email("Ecommerce","Thank you for your order " + orderId + "!\nWe are processing your order!");
-                    emailDispacher.send("ECOMMERCE_SEND_EMAIL", email, id, emailMessage);
                 }
-            }
         }
     }
 
