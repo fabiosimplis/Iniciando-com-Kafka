86de006cbd87bf1eb5647a2e58254167af00dc6b
fabiosimplis <fabio.simplis@gmail.com>
2024-07-16 10:42:04 -0300

    ADD ConsumerService no EmailNewOrderService

--- service-email-new-order/src/main/java/br/com/fjunior/ecommerce/EmailNewOrderService.java
+++ service-email-new-order/src/main/java/br/com/fjunior/ecommerce/EmailNewOrderService.java
@@ -1,30 +1,24 @@
 package br.com.fjunior.ecommerce;
 
-import br.com.fjunior.ecommerce.consumer.KafkaService;
+import br.com.fjunior.ecommerce.consumer.ConsumerService;
+import br.com.fjunior.ecommerce.consumer.ServiceRunner;
 import br.com.fjunior.ecommerce.dispacher.KafkaDispatcher;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order> {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args)  {
+        new ServiceRunner(EmailNewOrderService::new).start(1);
 
-        var fraudService = new EmailNewOrderService();
-        try(var service = new KafkaService<>(EmailNewOrderService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                fraudService::parse,
-                new HashMap<>())) {
-            service.run();
-        }
     }
 
     private final KafkaDispatcher<String> emailDispacher = new KafkaDispatcher<>();
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
 
         System.out.println("-----------------------------------------");
         System.out.println("Processing new order, preparing email");
@@ -36,6 +30,16 @@ public class EmailNewOrderService {
         emailDispacher.send("ECOMMERCE_SEND_EMAIL", order.getEmail(),
                 id,
                 emailCode);
+    }
+
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
     }
 
     private static boolean isFraud(Order order) {
